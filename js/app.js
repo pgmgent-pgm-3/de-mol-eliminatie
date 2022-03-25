95e89fd82915bbee7dbc70947a8617ced833fd0e
Frederick Roegiers <frederick@rogerthat.be>
2022-03-25 09:57:45 +0100

    wip

--- js/app.js
+++ js/app.js
@@ -3,13 +3,114 @@ let soundTicker;
 // initialize app when clicked on page (init sound)
 let appStarted = false;
 
-// array of all candidates
-let candidates = [
-  {
-    name: "Voorbeeldnaam",
-    score: 10,
-    time: 120,
-    isMol: false,
-    hasTicket: true,
-  },
+let candidateNames = [
+  "Jens",
+  "Yens",
+  "Uma",
+  "Toon",
+  "Sven",
+  "Philippe",
+  "Nele",
+  "Gretel",
+  "Emanuelle",
+  "Bert",
+  "Anke",
 ];
+
+let candidates = [];
+
+const makeRandomCandidateObjects = () => {
+  // assign a random mol
+  const molIndex = Math.floor(Math.random() * candidateNames.length);
+
+  // assign two random free pass candidates
+  const freePassInd1 = Math.floor(Math.random() * candidateNames.length);
+  const freePassInd2 = Math.floor(Math.random() * candidateNames.length);
+
+  candidates = candidateNames.map((candidate, index) => {
+    return {
+      name: candidate,
+      isMol: molIndex == index,
+      hasTicket: index == freePassInd1 || index == freePassInd2,
+      score: Math.round(Math.random() * 20),
+      time: Math.round(Math.random() * 120),
+    };
+  });
+};
+
+// assign random candidates
+makeRandomCandidateObjects();
+
+// DOM elements
+const btnStart = document.querySelector("#btnStart");
+const inpName = document.querySelector("#mol");
+const splashScreen = document.querySelector(".splash");
+
+inpName.onkeypress = (e) => {
+  if (e.key == "Enter") {
+    e.preventDefault();
+
+    const givenName = inpName.value.toLowerCase();
+    if (isCandidateOut(givenName)) {
+      showOverlay("red");
+      playSound("./sounds/afvaller.m4a");
+      soundTicker.volume = 0;
+    } else {
+      showOverlay("green");
+      playSound("./sounds/niet-afvaller.m4a");
+    }
+
+    inpName.value = "";
+  }
+};
+
+const isCandidateOut = (name) => {
+  return name == "chantal";
+};
+
+const showOverlay = (color) => {
+  // get overlay element
+  const overlay = document.querySelector(`#${color}`);
+  // remove hidden class ==> element becomes visible
+  overlay.classList.remove("hidden");
+
+  // revert to hidden state, unless it is red, then keep it bloody red!
+  if (color != "red") {
+    setTimeout(() => {
+      overlay.classList.add("hidden");
+    }, 5000);
+  }
+};
+
+btnStart.onclick = (e) => {
+  e.preventDefault();
+  // start app
+  removeSplashScreen();
+
+  // play intro
+  playSound("./sounds/intro.m4a");
+
+  // play ticking, with a delay of 10 seconds
+  setTimeout(playTicker, 10000);
+};
+
+// remove black screen with start btn
+const removeSplashScreen = () => {
+  splashScreen.style.display = "none";
+};
+
+// play some sound
+const playSound = (soundPath) => {
+  const sound = new Audio(soundPath);
+  sound.play();
+};
+
+const playTicker = () => {
+  soundTicker = new Audio("./sounds/getik.m4a");
+  soundTicker.volume = 0.7;
+  soundTicker.play();
+  soundTicker.onended = function (e) {
+    this.currentTime = 0;
+    this.play();
+  };
+};
