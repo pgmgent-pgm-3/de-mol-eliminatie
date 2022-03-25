5739866510506fe225bf049fe7ed809ed95db225
Frederick Roegiers <frederick@rogerthat.be>
2022-03-25 09:13:05 +0100

    init startcode

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let soundTicker;
+
+// initialize app when clicked on page (init sound)
+let appStarted = false;
+
+// array of all candidates
+let candidates = [
+  {
+    name: "Voorbeeldnaam",
+    score: 10,
+    time: 120,
+    isMol: false,
+    hasTicket: true,
+  },
+];
